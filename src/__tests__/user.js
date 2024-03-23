9c28bd0b70b7067ac6f0f1422c07e7ddb7548d11
JulieSoboleva <ju.soboll@gmail.com>
2024-03-23 14:42:35 +0300

    initial commit

---
+++ src/__tests__/user.js
@@ -0,0 +1,13 @@
+import loadUser from "../loadUser";
+import { httpGet } from "../http";
+jest.mock('../http');
+
+beforeEach(() => {  // перед каждым тестом (ещё есть afterEach, beforeAll, afterAll)
+    jest.resetAllMocks();  // сброс всех mocks
+});
+
+test('should call loadUser once', () => {
+    httpGet.mockReturnValue(JSON.stringify({}));
+    loadUser(1);
+    expect(httpGet).toBeCalledWith('http://server:8080/user/1');
+});
